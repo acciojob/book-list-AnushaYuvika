9dceea7f55242a41a4e4e4944f680504cb9965c1
Anusha Yuvika <151361373+AnushaYuvika@users.noreply.github.com>
2025-09-28 22:38:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 //your JS code here. If required.
+let btn = document.querySelector("#submit");
+
+btn.addEventListener("click", function() {
+	let title = document.querySelector("#title").value.trim();
+	let author = document.querySelector("#author");
+	let isbn = document.querySelector("#isbn");
+
+	if(title === "" || author === "" || isbn === "") {
+		alert("Please fill all fields");
+		return;
+	}
+
+	let tableBody = document.querySelector("#book-list");
+	let row = document.createElement("tr");
+
+	row.innerHTML = `
+	<td>${title}</td>
+	<td>${author}
+	<td>${isbn}</td>
+	<td><button class="delete">X</button>`;
+
+	tableBody.appendChild(row);
+
+	document.querySelector("#title").value = "";
+	document.querySelectro("#author").value = "";
+	document.querySelector("#isbn").value= "";
+});
+
+
+// document.querySelector("#book-list").addEventListener("click", function(e) {
+// 	if(e.target.classList.contains("delete")) {
+// 		e.target.parentElement.parentElement.remove();
+// 	}	
+// });
