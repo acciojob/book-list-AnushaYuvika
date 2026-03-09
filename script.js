1a3fdfa8480d396dfd735e8a4c16158cda9876d9
Anusha Yuvika <151361373+AnushaYuvika@users.noreply.github.com>
2026-03-09 16:45:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 //your JS code here. If required.
+const form = document.getElementById("book-form")
+const title = document.getElementById("title");
+const author = document.getElementById("author");
+const isbn = document.getElementById("isbn");
+const booklist = document.getElementById("book-list");
+
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+	const row = document.createElement("tr");
+
+	const td1 = document.createElement("td");
+	td1.textContent = title.value;
+
+	const td2 = document.createElement("td");
+	td2.textContent = author.value;
+
+	const td3 = document.createElement("td");
+	td3.textContent = isbn.value;
+
+	const btn = document.createElement("td");
+	btn.textContent = "❌";
+
+	row.appendChild(td1);
+	row.appendChild(td2);
+	row.appendChild(td3);
+	row.appendChild(btn);
+
+	booklist.appendChild(row);
+
+	btn.addEventListener("click", () => {
+		row.remove();
+	})
+	btn.style.cursor = "pointer";
+
+	form.reset();
+})
